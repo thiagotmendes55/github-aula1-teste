bc454931e59c71f8639346972f7e741bbabed933
thiagotmendes55 <thiagotmendes@gmail.com>
2020-03-15 10:39:17 -0300

    Manutencao realizada em casa

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -5,6 +5,7 @@ public class Programa {
 	public static void main(String[] args) {
 		System.out.println("Aula de github 1");
 		System.out.println("Manutencao de teste");
+		System.out.println("Manutencao feita em casa após puxar do GitHub");
 	}
 
 }
